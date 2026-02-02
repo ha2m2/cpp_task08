895a5371219762759105d491ce06f81faa757a45
ha2m2 <hamin0691@gmail.com>
2026-02-02 23:54:54 +0900

    init task08

---
+++ Source/CppStudy/Private/Item.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Item.h"
+
+// Sets default values
+AItem::AItem()
+{
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
+
+	/*static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/Resources/Props/SM_Star_C.SM_Star_C"));
+
+	if (MeshAsset.Succeeded()) {
+		StaticMeshComp->SetStaticMesh(MeshAsset.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UMaterial> MaterialAsset(TEXT("/Game/Resources/Materials/M_Star.M_Star"));
+	if (MaterialAsset.Succeeded()) {
+		StaticMeshComp->SetMaterial(0, MaterialAsset.Object);
+	}*/
+
+	PrimaryActorTick.bCanEverTick = true;
+	RotationSpeed = 45.f;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnItemPickedUp();
+
+	SetActorLocation(FVector(300.f, 200.f, 100.f));
+	SetActorRotation(FRotator(0.f, 45.f, 0.f));
+	SetActorScale3D(FVector(2.f));
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!FMath::IsNearlyZero(RotationSpeed)) {
+		AddActorLocalRotation(FRotator(0.f, RotationSpeed * DeltaTime, 0.f));
+	}
+}
+
+void AItem::ResetActorPosition()
+{
+	SetActorLocation(FVector::ZeroVector);
+}
+
+float AItem::GetRotationSpeed() const
+{
+	return RotationSpeed;
+}
+
---
+++ Source/CppStudy/Private/SpartaPlayerController.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpartaPlayerController.h"
+#include "EnhancedInputSubsystems.h" // 최신 인풋 시스템 불러오기
+
+ASpartaPlayerController::ASpartaPlayerController()
+	: InputMappingContext(nullptr),
+	MoveAction(nullptr),
+	JumpAction(nullptr),
+	LookAction(nullptr),
+	SprintAction(nullptr)
+{
+}
+
+void ASpartaPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ULocalPlayer* LocaPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem
+			= LocaPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (InputMappingContext) {
+				Subsystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
